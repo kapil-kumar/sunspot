8dce7d74681d3f76e7dac7017e457575eb21b37c
kapil kumar <kapil.kurmam@senecaglobal.com>
2015-05-13 17:15:38 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suntest::Application.config.secret_token = '47f5920424a6c605f0b5c12c89763445acbf00c5ef62bcf03a85b03491643580e082f42134995f93a855e9b183b325f316b8df0e1952e16be53bbaeb7b57c56c'
